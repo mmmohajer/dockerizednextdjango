1967aa77b052775e09e50bbf7571e26af2e2b45f
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-08-25 14:09:11 -0400

    Table updated

--- client/src/baseComponents/Pagination/Pagination.js
+++ client/src/baseComponents/Pagination/Pagination.js
@@ -6,28 +6,31 @@ import Icon from '@/baseComponents/Icon';
 
 import styles from './Pagination.module.scss';
 
-const Pagination = ({ currentPage, setCurrentPage, ...props }) => {
+const Pagination = ({ currentPage, setCurrentPage, numberOfTotalPages, ...props }) => {
   const firstPageComp = () => <Icon type="angleDoubleLeft" />;
   const lastPageComp = () => <Icon type="angleDoubleRight" />;
   const prevComp = () => <Icon type="angleLeft" />;
   const nextComp = () => <Icon type="angleRight" />;
   return (
     <>
-      <BasePagination
-        numberOfShownPages={5}
-        currentPage={currentPage}
-        setCurrentPage={setCurrentPage}
-        showDefaultFirstLastIcon={false}
-        showDefaultPrevNextIcon={false}
-        firstPageComp={firstPageComp}
-        lastPageCome={lastPageComp}
-        prevComp={prevComp}
-        nextComp={nextComp}
-        containerClassName={cx('')}
-        itemClassName={cx('')}
-        activeItemClassName={cx('')}
-        {...props}
-      />
+      {numberOfTotalPages > 1 && (
+        <BasePagination
+          numberOfShownPages={5}
+          numberOfTotalPages={numberOfTotalPages}
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          showDefaultFirstLastIcon={false}
+          showDefaultPrevNextIcon={false}
+          firstPageComp={firstPageComp}
+          lastPageCome={lastPageComp}
+          prevComp={prevComp}
+          nextComp={nextComp}
+          containerClassName={cx('')}
+          itemClassName={cx('')}
+          activeItemClassName={cx('')}
+          {...props}
+        />
+      )}
     </>
   );
 };
