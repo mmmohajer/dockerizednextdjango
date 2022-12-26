75eba308c7046b175da8460e74bfda2459cd4a5f
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-12-26 00:30:29 -0500

    minor fixes

---
+++ client/src/pages/register/index.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import cx from 'classnames';
+
+import RegisterComponent from '@/baseComponents/RegisterComponent';
+import PublicRoute from '@/components/PublicRoute';
+import Seo from '@/components/Seo';
+
+import styles from './Index.module.scss';
+
+const Index = () => {
+  return (
+    <PublicRoute>
+      <Seo>
+        <RegisterComponent />
+      </Seo>
+    </PublicRoute>
+  );
+};
+
+export default Index;
