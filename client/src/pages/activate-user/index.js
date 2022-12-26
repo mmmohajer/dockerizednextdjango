764943ffa445f9e124adc6bd84e125ee5bb810cd
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2022-12-26 00:32:23 -0500

    minor fixes

---
+++ client/src/pages/activate-user/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import cx from 'classnames';
+
+import ActivateUserComponent from '@/baseComponents/ActivateUser';
+import PublicRoute from '@/components/PublicRoute';
+import Seo from '@/components/Seo';
+
+import styles from './Index.module.scss';
+
+const Index = () => {
+  return (
+    <PublicRoute>
+      <Seo>
+        <ActivateUserComponent />
+      </Seo>
+    </PublicRoute>
+  );
+};
+
+export function getStaticParams() {
+  return ['token'];
+}
+
+export default Index;
