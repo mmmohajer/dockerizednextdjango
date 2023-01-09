4f3bbff46b833e716b49757a7dfbcffe7f2e4622
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-01-09 13:48:59 -0500

    Change in deploy script

--- utils/shellScripting/funcs/deploy.sh
+++ utils/shellScripting/funcs/deploy.sh
@@ -5,8 +5,6 @@ git add .
 git commit -m "$commitMsg"
 git push origin master
 local script=$( cat << EOF
-eval "$(ssh-agent -s)"
-ssh-add ~/github_rsa
 cd /var/www/app;
 git pull origin master;
 EOF
