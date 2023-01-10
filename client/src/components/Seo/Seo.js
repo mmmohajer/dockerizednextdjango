98e37907a1792d27f43720aeb380be0d540ec72f
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-01-09 20:00:34 -0500

    Input focus zoom in mobile prevented

--- client/src/components/Seo/Seo.js
+++ client/src/components/Seo/Seo.js
@@ -8,7 +8,7 @@ const Seo = ({ title, keywords, description, children }) => {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
         <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
         <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png" />
