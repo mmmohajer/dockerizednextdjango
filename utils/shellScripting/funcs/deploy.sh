e613d0423ce268b2f9b3df5aaa6917a5925a0f0c
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-01-10 13:46:24 -0500

    Automatic deploy updated

--- utils/shellScripting/funcs/deploy.sh
+++ utils/shellScripting/funcs/deploy.sh
@@ -1,26 +1,46 @@
 deploy() {
-local commitMsg=$(readData "What is your commit message to git?")
 getConfirmation "Are you willing to push it to github?"
 local pushToGithub=$?
-getConfirmation "Are you willing to completely remove all docker images in server, before building the new images?"
-local mustRemoveAllDockerImages=$?
+
 getConfirmation "Are you willing to build client folder in your local?"
-if [ $mustRemoveAllDockerImages -eq 0 ] 
+local buildClientInLocal=$?
+
+getConfirmation "Are you willing to deploy all new changes in the server?"
+local mustBuildNewChangesInServer=$?
+
+if [ $buildClientInLocal -eq 0 ]
 then
-    echo "Hello"
+    buildClient
 fi
-# buildClient
-# git add .
-# git commit -m "$commitMsg"
-# git push origin master
-# local script=$( cat << EOF
-# cd /var/www/app;
-# git pull origin master;
-# docker container rm -f $(docker container ls -a -q)
-# docker image rm -f $(docker image ls -a -q)
-# docker-compose -f docker-compose-prod-ssl.yml down;
-# docker-compose -f docker-compose-prod-ssl.yml up --build -d;
-# EOF
-# )
-# ssh $SERVER_ALIAS "$script" 
+if [ $pushToGithub -eq 0 ]
+then
+    local commitMsg=$(readData "What is your commit message to git?")
+    git add .
+    git commit -m "$commitMsg"
+    git push origin master
+fi
+
+if [ $mustBuildNewChangesInServer -eq 0 ]
+then
+local script=$( cat << EOF
+cd /var/www/app;
+git pull origin master;
+docker container rm -f $(docker container ls -a -q)
+docker image rm -f $(docker image ls -a -q)
+docker-compose -f docker-compose-prod-ssl.yml down;
+docker-compose -f docker-compose-prod-ssl.yml up --build -d;
+EOF
+)
+ssh $SERVER_ALIAS "$script" 
+fi
+
+if [ $mustBuildNewChangesInServer -ne 0 ]
+then
+local script=$( cat << EOF
+cd /var/www/app;
+git pull origin master;
+EOF
+)
+ssh $SERVER_ALIAS "$script" 
+fi
 }
