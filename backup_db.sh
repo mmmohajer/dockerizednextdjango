2f03059ccb7e614cfd57862876b4172425bd12e7
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-02-09 22:24:43 -0500

    Added db backup shell script

--- backup_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker exec app_db_1 pg_dump db -U mmmohajer70 > /var/www/app/db_backups/db_backup_$(date +%Y-%m-%d_%H-%M-%S).sql
-
-echo "Create a log"
-echo "Successfuly made a bckup from db on: $(date)" >> /home/mmmohajer70/cron_commands.log
