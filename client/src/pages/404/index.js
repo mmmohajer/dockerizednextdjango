f7d486677cc171dfebf3e944a3f627a7b822a14a
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-02-14 14:15:46 -0500

    API Doc page added

--- client/src/pages/404/index.js
+++ client/src/pages/404/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import cx from 'classnames';
 import { Div } from 'basedesign-iswad';
 
+import DivMinFullHeight from '@/baseComponents/DivMinFullHeight';
 import PublicRoute from '@/components/PublicRoute';
 import Seo from '@/components/Seo';
 import PageContainer from '@/components/PageContainer';
@@ -13,7 +14,9 @@ const Index = () => {
     <PublicRoute>
       <Seo>
         <PageContainer>
-          <Div>Unknown404</Div>
+          <DivMinFullHeight type="flex" hAlign="center" vAlign="center" className="fs-r-2">
+            <Div className="p4 bgThemeOne textWhite br-rad-px-10">Page Not Found!</Div>
+          </DivMinFullHeight>
         </PageContainer>
       </Seo>
     </PublicRoute>
--- client/src/pages/dev-pages/api-doc/index.js
+++ client/src/pages/dev-pages/api-doc/index.js
@@ -3,21 +3,24 @@ import cx from 'classnames';
 import { Div } from 'basedesign-iswad';
 
 import PublicRoute from '@/components/PublicRoute';
+import RoleBasedRoute from '@/components/RoleBasedRoute';
 import Seo from '@/components/Seo';
 import PageContainer from '@/components/PageContainer';
 import ApiDoc from '@/components/ApiDoc';
+
+import { USER_GROUPS } from '@/constants/userGroups';
 
 import styles from './Index.module.scss';
 
 const Index = () => {
   return (
-    <PublicRoute>
+    <RoleBasedRoute hasAccessRole={[USER_GROUPS.APP_ADMIN, USER_GROUPS.DEVELOPER]}>
       <Seo>
         <PageContainer hasHeader={false} hasFooter={false}>
           <ApiDoc />
         </PageContainer>
       </Seo>
-    </PublicRoute>
+    </RoleBasedRoute>
   );
 };
 
