7c8d67da4a70f02773f8df5d771d59eeb1649f7d
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-03-30 17:39:55 -0400

    Sidebar dashboard updated and is working pretty good

--- client/src/services/testimonials.js
+++
@@ -1,5 +0,0 @@
-import { setTestimonials as setTestmonialsReducer } from '@/reducers/apiCalls/testimonials';
-
-export const setTestimonials = (dispatch, data) => {
-  dispatch(setTestmonialsReducer(data));
-};
