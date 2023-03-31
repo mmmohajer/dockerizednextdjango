e2dae414ea81fa25d46f282935d62b59f7e25dbc
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-03-30 23:12:10 -0400

    Use backupdb in local added

--- use_backup_db.sh
+++ use_backup_db.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker exec dockerizednextdjango_db_1 psql -U devuser -d devdb < ./db_backups/db_backup_from_local_2023-03-30_17-52-34.sql
+docker exec web_db_1 psql -U devuser -c "DROP DATABASE devdb"
+docker exec web_db_1 psql -U devuser -d devdb -f ./db_backups/db_backup_from_local_2023-03-30_17-52-34.sql
