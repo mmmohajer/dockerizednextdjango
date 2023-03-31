105884effb62ebb811d010db7b58c47fa04b59b0
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-03-30 23:35:49 -0400

    Use backupdb in local added

--- utils/shellScripting/funcs/initialSetup.sh
+++ utils/shellScripting/funcs/initialSetup.sh
@@ -6,8 +6,6 @@ initialSetupInMAcOrLinux() {
     pip install pytest pytest-django model_bakery pytest-watch isort flake8 autopep8 locust python-dotenv
     echo "Python virtual environment is ready"
     cp ".env.sample" ".env"
-    cp "backup_db.sh.sample" "backup_db.sh"
-    cp "init-letsencrypt.sh.sample" "init-letsencrypt.sh"
     cp "config/envFiles/django/develop/.env.sample" "config/envFiles/django/develop/.env"
     cp "config/envFiles/django/prod/.env.sample" "config/envFiles/django/prod/.env"
     cp "config/envFiles/postgres/develop/.env.sample" "config/envFiles/postgres/develop/.env"
@@ -27,9 +25,6 @@ initialSetupInMAcOrLinux() {
 
 initialSetupInWindows() {
     cp ".env.sample" ".env"
-    cp "backup_db.sh.sample" "backup_db.sh"
-    cp "use_backup_db.sh.sample" "use_backup_db.sh"
-    cp "init-letsencrypt.sh.sample" "init-letsencrypt.sh"
     cp "config/envFiles/django/develop/.env.sample" "config/envFiles/django/develop/.env"
     cp "config/envFiles/django/prod/.env.sample" "config/envFiles/django/prod/.env"
     cp "config/envFiles/postgres/develop/.env.sample" "config/envFiles/postgres/develop/.env"
