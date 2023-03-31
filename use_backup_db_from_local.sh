2d57ee3ceeb3ef90768a13a213bdc7ed01d3811c
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2023-03-31 00:17:01 -0400

    Use backupdb in local added

--- use_backup_db_from_local.sh
+++ use_backup_db_from_local.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker exec web_db_1 psql -U devuser -c "DROP DATABASE devdb"
-docker exec web_db_1 psql -U devuser -d devdb -f ./db_backups/db_backup_from_local_2023-03-30_17-52-34.sql
+docker exec dockerizednextdjango_db_1 psql -U devuser -d devdb -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+docker exec dockerizednextdjango_db_1 psql -U devuser -d devdb -f ./db_backups/db_backup_from_local_2023-03-30_17-52-34.sql
