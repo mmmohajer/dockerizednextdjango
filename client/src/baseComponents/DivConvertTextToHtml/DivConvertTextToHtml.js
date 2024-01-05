3b21392d8ebef1d104a006711a45dfd1b007bb93
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2024-01-05 10:18:05 -0500

    Text component can show html content

--- client/src/baseComponents/DivConvertTextToHtml/DivConvertTextToHtml.js
+++ client/src/baseComponents/DivConvertTextToHtml/DivConvertTextToHtml.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import cx from 'classnames';
+import DOMPurify from 'dompurify';
 import { Div } from 'basedesign-iswad';
 
 import styles from './DivConvertTextToHtml.module.scss';
 
 const DivConvertTextToHtml = ({ text, ...props }) => {
+  const sanitizedHTML = DOMPurify.sanitize(text);
+
   return (
     <>
-      <Div dangerouslySetInnerHTML={{ __html: text }} {...props} />
+      <Div dangerouslySetInnerHTML={{ __html: sanitizedHTML }} {...props} />
     </>
   );
 };
--- client/src/baseComponents/Text/Text.js
+++ client/src/baseComponents/Text/Text.js
@@ -3,6 +3,7 @@ import cx from 'classnames';
 import { Div, Text as BaseText } from 'basedesign-iswad';
 
 import Icon from '@/baseComponents/Icon';
+import DivConvertTextToHtml from '../DivConvertTextToHtml';
 
 import { COLORS } from '@/constants/vars';
 
@@ -14,7 +15,7 @@ const Text = ({
   countJump = 1,
   initialTextContainerHeight = '100px',
   showViewAll = true,
-  iconColor = COLORS.themeFour,
+  iconColor = COLORS.themeTwo,
   className,
   ...props
 }) => {
@@ -44,7 +45,7 @@ const Text = ({
       setShownText(arrayOfVocabs.slice(0, count).join(' '));
       if (textRef.current.scrollHeight > textRef.current.clientHeight) {
         setMustBeTruncated(true);
-        setShownText(arrayOfVocabs.slice(0, count - 2 * countJump).join(' '));
+        setShownText(arrayOfVocabs.slice(0, count - 5 * countJump).join(' '));
         setIsTruncated(true);
       } else {
         if (count + countJump < arrayOfVocabs.length) {
@@ -68,7 +69,7 @@ const Text = ({
     if (isTruncated) {
       setTextContainerHeight(initialTextContainerHeight);
     } else {
-      setTextContainerHeight(textRef?.current?.scrollHeight);
+      setTextContainerHeight(textRef?.current?.scrollHeight + 20);
     }
   }, [isTruncated]);
 
@@ -79,7 +80,7 @@ const Text = ({
         style={{ height: textContainerHeight }}
         className={cx('pos-rel w-per-100 pos-rel', styles.textContainer, className)}
         {...props}>
-        {shownText}
+        <DivConvertTextToHtml text={shownText} style={{ display: 'inline' }} />
         {mustBeTruncated && isTruncated && !showViewAll ? <>...</> : ''}
         {mustBeTruncated && isTruncated && showViewAll ? (
           <>
@@ -104,7 +105,7 @@ const Text = ({
               onClick={() => {
                 setIsTruncated(true);
                 setTimeout(() => {
-                  setShownText(arrayOfVocabs.slice(0, count - 2 * countJump).join(' '));
+                  setShownText(arrayOfVocabs.slice(0, count - 5 * countJump).join(' '));
                 }, 300);
               }}>
               <Icon isBlock={false} type="minus-circle" color={iconColor} scale={1.2} />
