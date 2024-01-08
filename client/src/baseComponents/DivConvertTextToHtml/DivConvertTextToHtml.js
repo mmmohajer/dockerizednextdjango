437d943a602bd7996a53ed61ae1df7a2e2298b55
Mohammad Mahdi Mohajer <mmmohajer70@gmail.com>
2024-01-08 12:01:29 -0500

    Text component can show html content

--- client/src/baseComponents/DivConvertTextToHtml/DivConvertTextToHtml.js
+++ client/src/baseComponents/DivConvertTextToHtml/DivConvertTextToHtml.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import cx from 'classnames';
-import DOMPurify from 'dompurify';
+import * as DOMPurify from 'dompurify';
 import { Div } from 'basedesign-iswad';
 
 import styles from './DivConvertTextToHtml.module.scss';
 
 const DivConvertTextToHtml = ({ text, ...props }) => {
-  const sanitizedHTML = DOMPurify.sanitize(text);
+  const [sanitizedHtml, setSanitizedHtml] = useState('');
+  useEffect(() => {
+    if (text) {
+      setSanitizedHtml(DOMPurify.sanitize(text));
+    }
+  }, [text]);
 
   return (
     <>
-      <Div dangerouslySetInnerHTML={{ __html: sanitizedHTML }} {...props} />
+      <Div dangerouslySetInnerHTML={{ __html: sanitizedHtml }} {...props} />
     </>
   );
 };
